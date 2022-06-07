0102f63a1c6abaa2d71a985dd1fbf7c318af8da3
Danny-Quezada <danny.quezada32u@std.uni.edu.ni>
2022-06-07 08:39:55 -0600

    uploading c# api

---
+++ apiLibreria/Controllers/LibrosController.cs
@@ -0,0 +1,83 @@
+﻿using apiLibreria.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ViewModel;
+
+namespace apiLibreria.Controllers
+{
+    public class LibrosController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            List<LibrosWiewModel> librosWiewModels = new List<LibrosWiewModel>();
+
+            using (LibreriaEntities db = new LibreriaEntities())
+            {
+                librosWiewModels = (from l in db.Libros
+                                    select new LibrosWiewModel
+                                    {
+                                        Id = l.Id,
+                                        ISBN = l.ISBN,
+                                        Titulo = l.Titulo,
+                                        Autor = l.Autor,
+                                        Temas = l.Temas,
+                                        Editorial = l.Editorial,
+                                    }
+                                 ).ToList();
+
+                return Ok(librosWiewModels);
+            }
+
+        }
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            LibrosWiewModel librosWiewModel = null;
+
+            using (LibreriaEntities db = new LibreriaEntities())
+            {
+                librosWiewModel = db.Libros.Where(l => l.Id == id).Select(l => new LibrosWiewModel()
+                {
+                    Id = l.Id,
+                    ISBN = l.ISBN,
+                    Titulo = l.Titulo,
+                    Autor = l.Autor,
+                    Temas = l.Temas,
+                    Editorial = l.Editorial,
+                }).FirstOrDefault();
+            }
+
+            if (librosWiewModel == null)
+                return NotFound();
+            return Ok(librosWiewModel);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add(LibrosWiewModel librosWiewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("this is not model valid");
+            }
+            using(LibreriaEntities db =new LibreriaEntities())
+            {
+                var oLibro = new Models.Libros();
+                oLibro.ISBN = librosWiewModel.ISBN;
+                oLibro.Titulo = librosWiewModel.Titulo;
+                oLibro.Autor = librosWiewModel.Autor;
+                oLibro.Temas = librosWiewModel.Temas;
+                oLibro.Editorial = librosWiewModel.Editorial;
+                
+                db.Libros.Add(oLibro);
+                db.SaveChanges();
+            }
+            return Ok("Successfully added record");
+        }
+    }
+}
